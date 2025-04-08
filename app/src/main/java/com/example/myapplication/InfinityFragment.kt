9c2e1af722f54f007ebb58707d43f89bca434d54
AndresFelipe_s <info@lasus.com.co>
2025-04-08 15:46:10 -0500

    Refactorización de la navegación inferior y ajustes en los íconos. Se eliminan elementos innecesarios en InfinityFragment y se crea un nuevo layout para la barra de navegación. Se añaden títulos a los elementos del menú de navegación.

--- app/src/main/java/com/example/myapplication/InfinityFragment.kt
+++ app/src/main/java/com/example/myapplication/InfinityFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 
 
 private const val ARG_PARAM1 = "param1"
@@ -29,20 +28,8 @@ class InfinityFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_infinity, container, false)
 
-        // Configurar el click listener para el texto "Crear Cuenta"
-        val crearCuentaTextView = view.findViewById<TextView>(R.id.textView5)
-        crearCuentaTextView.setOnClickListener {
-            // Navegar al fragmento de registro
-            val registroFragment = InfinnityFragment_02()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, registroFragment)
-                .addToBackStack(null)
-                .commit()
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_infinity, container, false)
     }
 
     companion object {
